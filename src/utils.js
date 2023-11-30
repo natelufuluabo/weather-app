f58ea3640e603214fe1d1e948f5c86561983d06c
aircongo <natelufuluabo@yahoo.ca>
2023-11-30 15:07:37 -0500

    implemented the function that will be used to convert temperature from kelvin to celsius

--- src/utils.js
+++ src/utils.js
@@ -5,25 +5,25 @@ export async function handleChange(userChoice, cityCoordinates, location) {
         case "Montréal":
             location.lat = montreal.lat;
             location.long = montreal.long;
-            fetchWeatherData(location.lat, location.long);
+            await fetchWeatherData(location.lat, location.long);
             break;
             
         case "Québec":
             location.lat = quebec.lat;
             location.long = quebec.long;
-            fetchWeatherData(location.lat, location.long);
+            await fetchWeatherData(location.lat, location.long);
             break;
             
         case "Laval":
             location.lat = laval.lat;
             location.long = laval.long;
-            fetchWeatherData(location.lat, location.long);
+            await fetchWeatherData(location.lat, location.long);
             break;
             
         default:
             location.lat = currPos.lat;
             location.long = currPos.long;
-            fetchWeatherData(location.lat, location.long);
+            await fetchWeatherData(location.lat, location.long);
             break;
     }
 }
@@ -43,12 +43,25 @@ export function getFrenchFormattedDate() {
     return formatter.format(date);
 }
 
-export async function fetchWeatherData(latitude, longitude) {
+async function fetchWeatherData(latitude, longitude) {
     try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=1fe2d8d13d2a307d425792b9cbaf9b12`);
+        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=1fe2d8d13d2a307d425792b9cbaf9b12`;
+        const response = await fetch(url);
         const data = await response.json();
         console.log(data.weather, data.main);
     } catch (error) {
         console.error(error.message);
     }
+}
+
+function kelvinToCelsius(tempKelvin) {
+    if (typeof tempKelvin !== 'number' || isNaN(tempKelvin)) {
+      throw new Error('Invalid input. Please provide a valid number for temperature in Kelvin.');
+    }
+  
+    const tempCelsius = tempKelvin - 273.15;
+  
+    const roundedCelsius = Math.round(tempCelsius);
+  
+    return roundedCelsius;
 }
