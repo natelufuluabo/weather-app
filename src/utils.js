99da184d2444527247509abe1766af54b6593ca4
aircongo <natelufuluabo@yahoo.ca>
2023-11-30 15:00:11 -0500

    implemented the function that will be used to fetch weather data from the openweather api

--- src/utils.js
+++ src/utils.js
@@ -1,25 +1,29 @@
-export function handleChange(userChoice, cityCoordinates, location) {
+export async function handleChange(userChoice, cityCoordinates, location) {
     const { montreal, laval, quebec, currPos } = cityCoordinates;
 
     switch (userChoice) {
         case "Montréal":
             location.lat = montreal.lat;
             location.long = montreal.long;
+            fetchWeatherData(location.lat, location.long);
             break;
-
+            
         case "Québec":
             location.lat = quebec.lat;
             location.long = quebec.long;
+            fetchWeatherData(location.lat, location.long);
             break;
-
+            
         case "Laval":
             location.lat = laval.lat;
             location.long = laval.long;
+            fetchWeatherData(location.lat, location.long);
             break;
-
+            
         default:
             location.lat = currPos.lat;
             location.long = currPos.long;
+            fetchWeatherData(location.lat, location.long);
             break;
     }
 }
@@ -37,4 +41,14 @@ export function getFrenchFormattedDate() {
     const formatter = new Intl.DateTimeFormat('fr-FR', options);
   
     return formatter.format(date);
+}
+
+export async function fetchWeatherData(latitude, longitude) {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=1fe2d8d13d2a307d425792b9cbaf9b12`);
+        const data = await response.json();
+        console.log(data.weather, data.main);
+    } catch (error) {
+        console.error(error.message);
+    }
 }
